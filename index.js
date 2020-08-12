2d139e3f7250aaecabe7da40bb4440b919648e83
Jed Boyle <jedboyle01@gmail.com>
2020-08-12 13:49:40 +0100

    Add libs middleware so static files are accessible.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
+const path = require('path');
 // const exphbs = require('express-handlebars');
 // const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'libs/css')));
+app.use(express.static(path.join(__dirname, 'libs/img')));
+
 
 
 const PORT = process.env.PORT || 8000;
