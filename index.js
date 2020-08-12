1effb8289c8259225c429016f279d0c120d9a028
Jed Boyle <jedboyle01@gmail.com>
2020-08-12 13:56:29 +0100

    Create Handlebars file structure and configuration. Also add first route to index.hbs.

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const path = require('path');
 // const exphbs = require('express-handlebars');
 // const fs = require('fs');
 
+// Middleware
 app.use(express.static(path.join(__dirname, 'libs/css')));
 app.use(express.static(path.join(__dirname, 'libs/img')));
 
+// Handlebars templates
+app.engine('.hbs', exphbs({extname: 'hbs', defaultLayout: 'main'}));
+app.set('view engine', '.hbs');
 
+// Routes
+app.get('/', (req, res) => res.render('index', context));
 
+// Server listener
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
